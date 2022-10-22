da089a5bea3c78204b749cb2309b55ae8a5252dd
Joe McCain III <jo3mccain@icloud.com>
2022-10-22 22:46:25 +0000

    Update

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,10 +3,33 @@
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="container mx-auto">
+<div class="flex min-h-full p-3">
 	<span>
-		<h1>
-		</h1>
+		The ecosystem represents a wholistic commitment to the user and to securing our digital futures. Each proposed solution is a highly optimized, well researched implementation 
+		of cutting-edge technologies. We hope to provide the foundation for a truly free digital future where users are simply limited by their creativity and finally fulfilling 
+		the promise of blockchain technology, with a robust financial system designed for everyday use. With our platform, we hope to inspire a generation of doers, building their 
+		digital environments to work for them rather than on them. As technologies reach expands exponentially by the day, it is more important than ever to consider the way your 
+		information is being processed. Global events such as the COVID-19 pandemic and Russia v. Ukraine conflict have only accelerated the rise of technology pushing developers into 
+		the spotlight while ensuring the rise of cloud platforms. Each of these come on the tail-end of the initial rollout of 5G technologies, providing service providers with ample 
+		opportunity to build, test, and optimize their cloud platforms further ensnaring the public within their grasp. Throughout the course of time, the old mom n’ pop shops have 
+		been pushed to the wayside. Service providers have seemingly forgotten about small businesses, charging obscene fees for their products. Worse of all, the government has failed 
+		to reign in tech behemoths enabling them to continue with their malicious practices regarding their usage of personal information. Blockchain and decentralized technologies 
+		have provided a solution for many of the issues common to traditional providers yet have continuously failed to uphold the true promise of the idea. Furthermore, recent defi 
+		catastrophes, such as Terra, have further polluted the public’s viewpoint. Sadly however, it seems that centralized service providers have strategically downplayed the significance 
+		of blockchain and decentralization while repeatedly copying its principals. One of the most recent examples happens to be in authentication with the advent of the FIDO Alliance 
+		PassKey which simply enables applications to access a single credential stored on a specific host. The FIDO Alliance is a group of mega-corporations such as Apple, Google, and 
+		Facebook all of which are companies currently pursuing at least one of their own variants to some blockchain technology. Rather than questioning the technology, question the 
+		source to discover where the digital landscape is truly heading, and I am certain you will find little comfort in the ethics and practices common in traditional institutions. 
+		Blockchain was initially developed with the intention of distributing the very computer or command line the creator was operating at the time enabling for a host of novel transactions 
+		to occur and introduced the first tangible of digital currency. Removing all the hardware from a computer while enabling it to retain the fundamental differences that ensure each 
+		experience is outfitted to the users’ specifications, such as with an operating system, is a daunting task yet one being actively explored. Deta Space and WASMCloud are some examples 
+		of more traditional mindsets being applied to this concept. While these projects may offer a few of the capabilities of our platform, none can compare with the security and usability 
+		envisioned for the ecosystem. Our reliance on quantum resilient cryptographic protocols ensures the fidelity of each interaction facilitated by our systems. Additionally, the 
+		underlying architecture empowers users immerse themselves in highly customized digital environments which they can use to benefit them in any number of ways. Our primary focus is on 
+		enabling educational and professional development, a concept best shown with the introduction of stacked-guidelines, professional pipelines, and the enterprise grade creative studio. 
+		Lastly, all users will be fully equipped with each of the tools required to regain and retain control over their digital entities. Users will be encouraged to register compatible 
+		devices to best suite their day to day needs or to enable them to monetize their excess resources, participating in either computational or data-oriented workloads prescribed by 
+		the injected device analyzer.
 	</span>
 </div>
 
