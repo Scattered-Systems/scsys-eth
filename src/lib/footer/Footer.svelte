2e38ba5a8f137b6aad5d8efc90764a2e21a56de2
Joe McCain III <jo3mccain@icloud.com>
2022-10-23 22:15:00 +0000

    Update

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -3,23 +3,23 @@
 </script>
 
 
-<footer class="bottom-0 relative flex w-full justify-between p-3">
-    <div class="grid gap-4 grid-flow-col auto-cols-max auto-rows-max">
-        <div>
+<footer class="relative p-3 w-full">
+    <div class="flex items-center justify-between p-3">
+        <div class="flex flex-wrap">
             <ul class="list-none">
                 <li class="hover:text-underline">
                     <a href="https://github.com/{info.misc.github}">GitHub</a>
                 </li>
             </ul>
         </div>
-        <div>
+        <div class="flex flex-wrap">
             <ul class="list-none">
                 <li class="hover:text-underline">
                     <a href="https://scsys.eth.limo">{ info.company.name }</a>
                 </li>
             </ul>
         </div>
-        <div>
+        <div class="flex flex-wrap">
             <ul class="list-none">
                 <li class="hover:text-underline">
                     <a href="https://twitter.com/{info.misc.twitter}">Twitter</a>
