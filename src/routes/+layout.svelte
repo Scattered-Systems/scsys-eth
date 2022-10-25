3fa7b36429b52d66f166d346102acebf8f62c3eb
Joe McCain III <jo3mccain@icloud.com>
2022-10-25 05:36:38 +0000

    Update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script>
 	import Footer from '$lib/footer/Footer.svelte';
 	import Navbar from '$lib/navbar/Navbar.svelte';
-	import { info } from '$lib/constants.js'
 	import "$lib/app.css";
 </script>
 
 <Navbar/>
-<div class="application bg-gradient-to-r from-zinc-900 via-zinc-800 to-zinc-900 text-white">
+<div class="application bg-gradient-to-br from-zinc-900 via-zinc-800 to-zinc-900 text-white">
 	<div class="flex flex-auto items-center justify-center mt-16 min-h-full">
 		<main class="container mx-auto min-h-full">
 			<slot />
