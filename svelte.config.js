013068d7d97e9d4a3b02064c037cf643313ced9d
Joe McCain III <jo3mccain@icloud.com>
2023-01-21 04:52:55 +0000

    Update

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,22 @@
 import adapter from '@sveltejs/adapter-node';
+import adapter_static from '@sveltejs/adapter-static';
+import preprocess from "svelte-preprocess";
+
 
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+		...(process.env.MODE === "static") && {
+			adapter: adapter_static()
+		},
+		...(process.env.MODE !== "static") && {
+			adapter: adapter()
+		}
+	},
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+	],
 };
 
 export default config;
